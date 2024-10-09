1dda610a48e07c685cbeb1949b74ac6a3adca81a
zilola0809 <zilola@gmail.com>
2024-10-09 16:54:03 +0500

    Create 8-work.py

---
+++ 8-work.py
@@ -0,0 +1,15 @@
+ismlar = ["Shahnoza", 'Shaydo', 'Shodiya', 'Oygul', 'Elif']
+for ism in ismlar:
+    print("Assalomu aleykum " + ( ism))
+#  2
+
+sonlar= list(range(10,100))
+for son in sonlar:
+    print(f"{son} ning kvadrati {son**2} ga teng")
+3
+#
+filmlar=[]
+print('5 ta eng sevimli filmingizni kiriting:')
+for n in range(5):
+    filmlar.append(input(f"{n+1}-kino nomini kiriting: "))
+print(filmlar)
